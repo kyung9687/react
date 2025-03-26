891d35469f1a51c360db6ec3862f0f4dfee7169b
shakeit <134345291+kyung9687@users.noreply.github.com>
2025-03-26 13:52:36 +0900

    react

---
+++ simple-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const Button = () =>{
+    return <button className='btn'>버튼</button>
+  }
+  
+  return( 
+<>
+  <h1>리엑트란?</h1>
+  <pre>
+
+    
+  </pre>
+  <Button/>
+  
+</>
+  );
+
+}
+
+export default App
